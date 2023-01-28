cf7e17e147801a3f74d57339f824576a790a7e0a
pettrop <p.tropp@gmail.com>
2023-01-28 16:36:06 +0100

    Added correct STATIC path in settings.py v4

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -142,10 +142,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'images/'
 MEDIA_ROOT = BASE_DIR / 'static/images'
@@ -173,5 +172,3 @@ PASSWORD_RESET_TIMING = 3600
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
