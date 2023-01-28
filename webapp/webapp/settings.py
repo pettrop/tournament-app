8e85c167434a566306daafe8e51c3f62ef752a79
pettrop <p.tropp@gmail.com>
2023-01-28 14:14:11 +0100

    Edit settings.py - added os enviroment variable

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#-h+(wf*ya3ds6c^xq625k%v=+-2di9-lt0-ze#+m*3o3m4t+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$#-h+(wf*ya3ds6c^xq625k%v=+-2di9-lt0-ze#+m*3o3m4t+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 ALLOWED_HOSTS = []
